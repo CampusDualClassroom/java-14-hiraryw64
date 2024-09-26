96a08ad2f7f4e64af70cd1f68995da0edea64fe8
hiraryw64 <hiraryw64@gmail.com>
2024-09-26 13:54:09 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Doctor.java
+++ src/main/java/com/campusdual/classroom/Doctor.java
@@ -1,11 +1,15 @@
 package com.campusdual.classroom;
 
 public class Doctor extends Person {
-
 	protected String specialization;
 
 	public Doctor(String name, String surname, String specialization) {
 		super(name, surname);
 		this.specialization = specialization;
+	}
+
+	@Override
+	public void getDetails() {
+		System.out.println("Name: " + name + "\nSurname: " + surname + "\nSpecialization: " + specialization);
 	}
 }
--- src/main/java/com/campusdual/classroom/Exercise14.java
+++ src/main/java/com/campusdual/classroom/Exercise14.java
@@ -4,9 +4,16 @@ public class Exercise14 {
 
     public static void main(String[] args) {
         Person p = new Person("John", "Smith");
+        p.getDetails();
+
         Teacher t = new Teacher("Maria", "Montessori", "Educación");
+        t.getDetails();
+
         PoliceOfficer po = new PoliceOfficer("Jake", "Peralta", "B-99");
+        po.getDetails();
+
         Doctor d = new Doctor("Gregory", "House", "Nefrología e Infectología");
+        d.getDetails();
     }
 
 }
